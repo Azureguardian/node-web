ee1b42257f34d1a3a5fb0a8c206cdb3040f4e574
Ilya Muromets <imuromec@npoprogress.com>
2018-06-09 14:09:07 +0300

    test make_readable dummy function

--- spec/make_readable_spec.js
+++ spec/make_readable_spec.js
@@ -2,10 +2,16 @@ var makeReadable = require("../make_readable.js");
 
 describe("Make_readable spec suite", function () {
     describe("Function returnes simplified HTML", function() {
-        var valid_html = "<html><body></body></html>";
+        var valid_html = "<html><body><div><h1>Article</h1></h1></div>></body></html>";
         it ("returns valid HTML", function () {
-            expect(makeReadable.make_readable(valid_html)).toBeDefined();
+            expect(makeReadable.make_readable(valid_html)).toContain('Article');
         })
     });
+    describe("test", function () {
+        it ('returns 4', function () {
+            expect(2+3).toEqual(5);
+        });
+    })
 });
+
 
